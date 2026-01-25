d085e86ea66be271b5b713a69fc0d46bf64d6233
dsecnet <dsecnet@gmail.com>
2026-01-25 14:18:39 +0400

    Buttona qeder

---
+++ MyIosApp/MyIosApp/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  MyIosApp
+//
+//  Created by Vusal Dadashov on 25.01.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+           
+            Text("Salam Vusal")                //ui element
+                .fontWeight(.black)
+                .font(.largeTitle)
+                .font(.system(.largeTitle, design: .monospaced ))
+            
+            //bunlar modifierdi
+            
+            Button {
+                //what it does
+            } label: {
+                //how it  looks
+                Text("klik et")
+                    .fontWeight(.bold)
+                    .font(.system(.title, design: .monospaced))
+            }
+        }
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyIosApp/MyIosApp/MyIosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyIosAppApp.swift
+//  MyIosApp
+//
+//  Created by Vusal Dadashov on 25.01.26.
+//
+
+import SwiftUI
+
+@main
+struct MyIosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
