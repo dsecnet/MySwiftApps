25a8084a04481d6c141096f6691ae5c5db76efe8
dsecnet <dsecnet@gmail.com>
2026-03-02 09:32:02 +0400

    IOS dersleri baslangic

--- IOS dersleri/RestoranApp/RestoranApp/ContentView.swift
+++ IOS dersleri/RestoranApp/RestoranApp/ContentView.swift
@@ -1,32 +1,45 @@
-//
-//  ContentView.swift
-//  RestoranApp
-//
-//  Created by Vusal Dadashov on 27.02.26.
-//
-
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
+    let synthesizer = AVSpeechSynthesizer()
+    // svg pdf sekilleri qoyuruq 
     var body: some View {
         VStack {
             Text("Salam, I am Vusal")
                 .fontWeight(.black)
                 .font(.largeTitle)
-            Button{
-                //what it does
-            } label: {
-                Text("klik et")
-                    .fontWeight(.bold)
-                    .font(.system(.title, design: .monospaced))
+           
+            HStack {
+                Button {
+                    let utterance = AVSpeechUtterance(string: "Salam, I am Vusal")
+                    utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+                    synthesizer.speak(utterance)
+                } label: {
+                    Text("klik et")
+                        .fontWeight(.bold)
+                        .font(.system(.title, design: .monospaced))
+                }
+                .padding(10)
+                .foregroundColor(.white)
+                .background(Color.purple)
+                .cornerRadius(6)
+
+                Button {
+                    let utterance = AVSpeechUtterance(string: "Salam, I am Vusal")
+                    utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+                    synthesizer.speak(utterance)
+                } label: {
+                    Text("Cancel")
+                        .fontWeight(.bold)
+                        .font(.system(.title, design: .monospaced))
+                }
+                .padding(10)
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .cornerRadius(6)
             }
-            .padding(10)
-            .foregroundColor(.white)
-            .background(Color.purple)
-            .cornerRadius(6)
-                
         }
-       
     }
 }
 
